43ad393138a6c494f128c1abb6c218fdd4f6f886
teakdemir <136896802+teakdemir@users.noreply.github.com>
2025-01-20 20:08:00 +0300

    eskiye dönüş

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,37 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy: MonoBehaviour
 {
-    [SerializeField] float health = 3f;
-    [SerializeField] float damageToPlayer = 1f;  // Düşmanın oyuncuya vereceği hasar
-    private float maxHealth;
+  [SerializeField] float health, maxhealth=3f;
 
-    private void Start()
+  private void Start(){
+
+health=maxhealth;
+
+  }
+
+public void TakeDamage(float damageAmount)
+{
+
+    health -= damageAmount;
+
+    if(health <= 0)
     {
-        maxHealth = health;
+        Destroy(gameObject);
     }
+}
 
-    public void TakeDamage(float damageAmount)
-    {
-        health -= damageAmount;
-
-        if (health <= 0)
-        {
-            Destroy(gameObject); // Düşman öldüğünde yok olur
-        }
-    }
-
-    // Düşmanla çarpışınca oyuncuya hasar ver
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            // Oyuncuya hasar ver
-            PlayerStats playerStats = collision.gameObject.GetComponent<PlayerStats>();
-            if (playerStats != null)
-            {
-                playerStats.TakeDamage(damageToPlayer); // Oyuncunun canını azalt
-            }
-        }
-    }
 }

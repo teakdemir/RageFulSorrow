991bb82d949873c447b4fd5b349f04a149ace3cd
ozgeekoc <ozgekoc2004@gmail.com>
2025-01-24 03:22:01 +0300

    Hemşirenin silahı eklendi

---
+++ Assets/Enemies/Nurse/ScissorsScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ScissorsScript : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float force;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
